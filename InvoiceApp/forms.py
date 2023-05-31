79c534a699c5132e4c22c60b5b2ed5e06c0917c7
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-05-31 17:27:31 +0300

    viska susidejau atgal

---
+++ InvoiceApp/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Client, Product, Service, Invoice
+
+
+class DateInputForm(forms.ModelForm):
+    input_type = 'date'
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = (
+            'client_name', 'registration_number', 'vat_number', 'address', 'email_address', 'phone_number',
+        )
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'product_name', 'product_code', 'product_description', 'product_quantity', 'product_price',
+        )
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = (
+            'service_name', 'service_code', 'service_description', 'service_quantity', 'service_price',
+        )
+
+class InvoiceForm(forms.ModelForm):
+
+    class Meta:
+        model = Invoice
+        fields = (
+            'invoice_name', 'invoice_number', 'due_date', 'payment_terms', 'invoice_status', 'client',
+            'product', 'service',
+        )
+        widgets = {'due_date': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),}
+
+
+
+# type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+class UserLoginForm(forms.ModelForm):
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'id': 'floatingInput', 'for':"floatingInput"}), required=True)
+    # password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'id': 'floatingPassword'}), required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'password',
+        )
---
+++ InvoiceApp/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import User
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from django.views import generic
+from .forms import UserLoginForm
+
+from django.contrib.auth.models import User, auth
+
+
+
+# Index view: loads register.html instead of some kind of index page
+def index(request):
+    context = {}
+    return render(request, 'registration/login.html', context)
+
+# register view
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        # getting values from register form
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        #position = request.POST['position']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # check if the passwords match
+        if password == password2:
+            # check if username exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                return redirect('register')
+            # check if email exists
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    return redirect('register')
+            # If those ifs passes, new user ir created
+                else:
+                    User.objects.create_user(
+                        username=username, first_name=first_name, last_name=last_name,
+                        email=email, password=password
+                    )
+                    messages.info(request, f'Vartotojas {username} užregistruotas!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Slaptažodžiai nesutampa! Bandykite dar kartą.')
+            return redirect('register')
+    return render(request, 'registration/register.html')
+
+
+def anonymous_required(function=None, redirect_url=None):
+    if not redirect_url:
+        redirect_url = 'dashboard'
+
+    actual_decorator = user_passes_test(lambda user: user.is_anonymous, login_url=redirect_url)
+    if function:
+        return actual_decorator
+
+@anonymous_required
+# # Index view: loads login.html instead of some kind of index page
+def login(request):
+    context = {}
+    if request.method == 'GET':
+        form = UserLoginForm()
+        context['form'] = form
+        return render(request, 'registration/login.html', context)
+
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            context['form'] = form
+            messages.error(request, 'Blogi prisijungimo duomenys')
+            return redirect('login')
+    return render(request, 'registration/login.html', context)
+
+@login_required
+def dashboard(request):
+    context = {}
+    return render(request, 'dashboard.html', context)

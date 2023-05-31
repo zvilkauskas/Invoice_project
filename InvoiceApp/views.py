5d1b13fdd7172901b49db67e44dd3dce26e48cbd
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-05-31 17:52:04 +0300

    tvarkomas html dashborad

--- InvoiceApp/views.py
+++ InvoiceApp/views.py
@@ -75,7 +75,7 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(email=email, password=password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)

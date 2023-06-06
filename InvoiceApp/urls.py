fbe79e3e5170edb9f1d24dc23fd88735bb09ab46
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-06-06 20:57:55 +0300

    papildyti urls su add funkcijom

--- InvoiceApp/urls.py
+++ InvoiceApp/urls.py
@@ -9,19 +9,11 @@ urlpatterns = [
     path('logged_out/', views.logged_out, name='logged_out'),
     path('main_page/', views.main_page, name='main_page'),
     path('clients/', views.clients, name='clients'),
-<<<<<<< HEAD
-    path('clients/add_client/', views.clients, name='add_client'),
-    path('products/', views.products, name='products'),
-    path('products/add_product/', views.products, name='add_product'),
-    path('services/', views.services, name='services'),
-    path('services/add_service/', views.services, name='add_service'),
-=======
     path('clients/add_client/', views.add_client, name='add_client'),
     path('products/', views.products, name='products'),
     path('products/add_product/', views.add_product, name='add_product'),
     path('services/', views.services, name='services'),
     path('services/add_service/', views.add_service, name='add_service'),
->>>>>>> views
     path('invoices/', views.invoices, name='invoices'),
     path('invoices/create_invoice', views.create_invoice, name='create_invoice'),
 ]

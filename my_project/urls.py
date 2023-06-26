6f124a7914351ee2be5f7f27c230c586522a0b2c
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-06-26 23:24:20 +0300

    forgot password bus veliau

--- my_project/urls.py
+++ my_project/urls.py
@@ -30,29 +30,5 @@ urlpatterns = [
     path('', include('InvoiceApp.index')),
     path('invoices/', include('InvoiceApp.urls')),
     path('', RedirectView.as_view(url='invoice/index', permanent=False)),
-# Forget Password
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password-reset/password_reset.html',
-             subject_template_name='registration/password-reset/password_reset_subject.txt',
-             email_template_name='registration/password-reset/password_reset_email.html',
-             # success_url='/login/'
-         ),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password-reset/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password-reset/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password-reset/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
 
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
